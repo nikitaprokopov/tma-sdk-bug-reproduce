3fb1668a2ce04b6fe0dc786368ef6594697a3f76
nikita <nktprggl@gmail.com>
2025-06-20 09:24:05 +0300

    try new fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,20 @@
 import { isTMA } from "@telegram-apps/sdk-react";
 import { useEffect, useState } from "react";
 
+function getIsTma() {
+  if (isTMA()) {
+    return isTMA("complete");
+  }
+
+  return false;
+}
+
 export function App() {
   const [isTmaResult, setIsTmaResult] = useState<boolean | null>(null);
 
   useEffect(() => {
     async function init() {
-      const isTma = await (isTMA() && isTMA("complete"));
+      const isTma = await getIsTma();
       setIsTmaResult(isTma);
     }
 
